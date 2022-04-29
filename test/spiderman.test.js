870bf3f79e1bc05c8af7c218d00d758239717201
Marco <macdev.gmail@gmail.com>
2022-04-28 22:41:10 -0500

    functional code in class spiderman and test passed

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -2,12 +2,12 @@ const Spiderman = require('../app/Spiderman');
 
 describe("Test Suite Dummy Description", () => {
   test('1. Create an spiderman object', () => {
-    const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield, 2", "Sony")
+    const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
 
     expect(andrewGarfield.name).toBe("Spiderman Sony");
     expect(andrewGarfield.age).toBe(31);
     expect(andrewGarfield.actor).toBe("Andrew Garfield");
-    expect(andrewGarfield.mocies).toBe(31);
+    expect(andrewGarfield.movies).toBe(2);
     expect(andrewGarfield.studio).toBe("Sony");
   });
 })
