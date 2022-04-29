8b76d58248509b97dcece6486de46cde606a33e8
Marco <macdev.gmail@gmail.com>
2022-04-28 22:21:06 -0500

    spiderman test created

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,11 @@
 describe("Test Suite Dummy Description", () => {
-  test('Case 1 Dummy', () => {
-    const resultOfSomething = 1 + 2
-    expect(resultOfSomething).toBe(18);
+  test('1. Create an spiderman object', () => {
+    const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield, 2", "Sony")
+
+    expect(andrewGarfield.name).toBe("Spiderman Sony");
+    expect(andrewGarfield.age).toBe(31);
+    expect(andrewGarfield.actor).toBe("Andrew Garfield");
+    expect(andrewGarfield.mocies).toBe(31);
+    expect(andrewGarfield.studio).toBe("Sony");
   });
 })
